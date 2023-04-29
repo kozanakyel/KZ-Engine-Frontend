73836b6b4fe1cda39afdd267fd84ec6f0b2c76a7
kozanakyel <kozanakyel@gmail.com>
2023-04-29 23:39:27 +0300

    fix: interval error

--- src/pages/engine/Engine.jsx
+++ src/pages/engine/Engine.jsx
@@ -2,47 +2,44 @@ import React from 'react';
 import { useLocation } from 'react-router-dom';
 
 import { useState, useEffect } from 'react';
-import axios from "../../utils/axios";
+import axios from '../../utils/axios';
 
-import Card from "../../components/card/Card";
-import Footer from "../../components/footer/Footer";
-
+import Card from '../../components/card/Card';
+import Footer from '../../components/footer/Footer';
 
 const Engine = () => {
+  const [allSignals, setAllSignals] = useState([]);
+  const location = useLocation();
+  const data = location.state;
 
-    const [allSignals, setAllSignals] = useState([])
-    const location = useLocation();
-    const data = location.state;
+  useEffect(() => {
+    axios
+      .post('/signal_tracker_all', {
+        interval: data?.interval, // get from header component button click
+        ai_type: 'XgboostForecaster', // default value
+      })
+      .then(function (response) {
+        setAllSignals(response.data);
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
+  }, []);
 
-    useEffect(() => {
-
-        axios.post("/signal_tracker_all", {
-            interval: data.interval,    // get from header component button click
-            ai_type: "XgboostForecaster"  // default value
-        })
-            .then(function (response) {
-                setAllSignals(response.data);
-            }).catch(function (error) {
-            console.log(error);
-        });
-    }, [])
-
-    return (
-        <>
-            {allSignals.map((signal) => (
-            <div key={signal.ticker} className={"hor-ver-centered m-3"}>
-
-                <Card
-                    signalTracker={signal}
-                    forecastModel={signal?.forecast_model}
-                    crypto={signal?.forecast_model?.crypto}
-                />
-
-            </div>
-            ))}
-            <Footer />
-        </>
-    )
-}
+  return (
+    <>
+      {allSignals.map((signal) => (
+        <div key={signal.ticker} className={'hor-ver-centered m-3'}>
+          <Card
+            signalTracker={signal}
+            forecastModel={signal?.forecast_model}
+            crypto={signal?.forecast_model?.crypto}
+          />
+        </div>
+      ))}
+      <Footer />
+    </>
+  );
+};
 
 export default Engine;
