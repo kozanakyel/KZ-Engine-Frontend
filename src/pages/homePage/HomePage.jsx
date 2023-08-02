b73175e6b109a502c8e337fe0f2faef98ae494e0
kozanakyel <kozanakyel@gmail.com>
2023-08-02 14:56:43 +0300

    edited home page

--- src/pages/homePage/HomePage.jsx
+++ src/pages/homePage/HomePage.jsx
@@ -1,31 +1,33 @@
-import React from "react";
-import Header from "@/components/header/Header";
-import Footer from "@/components/footer/Footer";
-import InfoSection from "@/components/infoSection/InfoSection";
-import KzengineDiagram from "@/components/kzengineDiagram/KzengineDiagram";
-import AIProjectAssistant from "../../components/aiProjectAssistant/AIProjectAssistant";
-import TradingAdvisor from "../../components/tradingAdvisor/TradingAdvisor";
-import KzaiAssistant from "../../components/kzaiAssistant/KzaiAssistant";
-import SentimentCircleChart from "../../components/sentimentCircleChart/SentimentCircleChart";
-import SentimentCard from "../../components/sentimentCard/SentimentCard";
-import JapaneseCandlestickChart from "../../components/japaneseCandlestickChart/JapaneseCandlestickChart";
+import React from 'react';
+import Header from '@/components/header/Header';
+import Footer from '@/components/footer/Footer';
+import InfoSection from '@/components/infoSection/InfoSection';
+import KzengineDiagram from '@/components/kzengineDiagram/KzengineDiagram';
+import AIProjectAssistant from '../../components/aiProjectAssistant/AIProjectAssistant';
+import TradingAdvisor from '../../components/tradingAdvisor/TradingAdvisor';
+import KzaiAssistant from '../../components/kzaiAssistant/KzaiAssistant';
+import SentimentCircleChart from '../../components/sentimentCircleChart/SentimentCircleChart';
+import SentimentCard from '../../components/sentimentCard/SentimentCard';
+import JapaneseCandlestickChart from '../../components/japaneseCandlestickChart/JapaneseCandlestickChart';
 
 const HomePage = () => {
-  const infoApp1 = (
+  const infoProjectPurposes = (
     <p className=" mb-3 text-light">
-
-      <li>Using Decision Trees, Sentiment Analysis with Natural Language Processing, and Japanese Candlestick Art.</li>
-      <li>Forecasting the structure of the next candle for popular cryptocurrencies like Bitcoin, Ethereum, and Binance
-        Coin.
+      <li>
+        Using Decision Trees, Sentiment Analysis with Natural Language
+        Processing, and Japanese Candlestick Art.
+      </li>
+      <li>
+        Forecasting the structure of the next candle for popular
+        cryptocurrencies like Bitcoin, Ethereum, and Binance Coin.
       </li>
       <li>For Bitcoin, Ethereum, and Binance Coin.</li>
       <li>Binary format prediction.</li>
       <li>Next candle for 1 hour and 1 day</li>
-
     </p>
   );
 
-  const infoSentiment = (
+  const infoSentimentTracker = (
     <p className=" mb-3 text-light">
       <li>Do data mining with twitter api.</li>
       <li>Graph algorithm for detecting twitter phenomena</li>
@@ -35,7 +37,7 @@ const HomePage = () => {
     </p>
   );
 
-  const kayzeeAsssitant = (
+  const infoKayzeeAsssitant = (
     <p className=" mb-3 text-light">
       <li>Powered to LLM process</li>
       <li>Langchain framework supported</li>
@@ -45,20 +47,33 @@ const HomePage = () => {
     </p>
   );
 
-
   return (
     <>
       <Header />
       <AIProjectAssistant />
-      <InfoSection info={infoApp1} head={"Project Purposes"} color={""} image={'/images/dmpurpose.png'}/>
-      <InfoSection info={infoSentiment} head={"Sentiment Tracker"} color={""} />
-      <InfoSection info={kayzeeAsssitant} head={"KayZee Assistant"} color={""} image={'/images/kzaiassist.png'}/>
+      <InfoSection
+        info={infoProjectPurposes}
+        head={'Project Purposes'}
+        color={''}
+        image={'/images/dmpurpose.png'}
+      />
+      <InfoSection
+        info={infoSentimentTracker}
+        head={'Sentiment Tracker'}
+        color={''}
+      />
+      <InfoSection
+        info={infoKayzeeAsssitant}
+        head={'KayZee Assistant'}
+        color={''}
+        image={'/images/kzaiassist.png'}
+      />
       {/* <SentimentCircleChart /> */}
       {/* <SentimentCard /> */}
       {/* <JapaneseCandlestickChart /> */}
       {/* <TradingAdvisor /> */}
       <KzengineDiagram />
-      
+
       {/* <KzaiAssistant /> */}
       <Footer />
     </>
